094ee1d08458a452f92c81928b53fbc39fc57eba
Pauline De Ro <pauline@bobex.net>
2022-11-21 09:35:25 +0100

    start doctor controller exercice

---
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -0,0 +1,28 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String firstDoctor() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String secondDoctor() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String thirdDoctor() {
+        return "Jodie Whittaker";
+    }
+}
